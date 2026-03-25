f91690db90efae91ce09a6798702783d6c7e0f1a
julien-scapini <julienscapini99@gmail.com>
2026-03-24 21:51:01 -0300

    alteração de correção de textos

--- personagem.js
+++ personagem.js
@@ -1,31 +1,30 @@
-let NomeUsuario = prompt("qual é o seu nome?");
-alert("olá " + NomeUsuario)
+let NomePersonagem = prompt("qual é o seu nome?");
+alert("olá " + NomePersonagem)
 let XpPersonagem = parseInt(prompt("quantos Pontos de Experiência seu personagem tem?"));
 
 if (XpPersonagem <= 1000) {
-    alert("seu personagem é da classe ferro");
+    alert("O personagem de nome " + NomePersonagem + " é da classe ferro");
 }
 else if (XpPersonagem > 1000 && XpPersonagem <= 2000) {
-    alert("seu personagem é da classe bronze");
-
+    alert("O personagem de nome " + NomePersonagem + " é da classe bronze");
 }
 
 else if (XpPersonagem > 2000 && XpPersonagem <= 5000) {
-    alert("seu personagem é da classe prata");
+    alert("O personagem de nome " + NomePersonagem + " é da classe prata");
 
 }
 else if (XpPersonagem > 5000 && XpPersonagem <= 7000) {
-    alert("seu personagem é da classe ouro");
+    alert("O personagem de nome " + NomePersonagem + " é da classe ouro");
 }
 else if (XpPersonagem > 7000 && XpPersonagem <= 8000) {
-    alert("seu personagem é da classe platina");
+    alert("O personagem de nome " + NomePersonagem + " é da classe platina");
 }
 else if (XpPersonagem > 8000 && XpPersonagem <= 9000) {
-    alert("seu personagem é da classe ascedente");
+    alert("O personagem de nome " + NomePersonagem + " é da classe ascedente");
 }
 else if (XpPersonagem > 9000 && XpPersonagem <= 10000) {
-    alert("seu personagem é da classe imortal");
+    alert("O personagem de nome " + NomePersonagem + " é da classe imortal");
 }
 else {
-    alert("seu personagem é da classe Radiante");
+    alert("O personagem de nome " + NomePersonagem + " é da classe Radiante");
 }
